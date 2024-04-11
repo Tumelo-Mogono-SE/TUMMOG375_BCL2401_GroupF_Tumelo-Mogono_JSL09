17e0e1e5c44c0575f010856fe62d3d9c5cc00cb1
Tumelo Mogono <tumelomogono@gmail.com>
2024-04-11 14:44:49 +0200

    fetch images from api to retrieve properties

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//Fetching a random image from Unsplash and setting it as the background
 
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        document.body.style.backgroundImage = `url(${data.urls.full})`;
+        document.getElementById('author').textContent = `By: ${data.user.name}`;
+    })
 

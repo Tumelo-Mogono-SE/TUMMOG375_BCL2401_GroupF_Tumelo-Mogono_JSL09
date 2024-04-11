a1f23cae78a146c6860f527144eb89824c8840f9
Tumelo Mogono <tumelomogono@gmail.com>
2024-04-11 19:49:33 +0200

    added function to get time and code to change time.

--- index.js
+++ index.js
@@ -11,26 +11,36 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1484766280341-87861644c80d?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4NDAzMTJ8&ixlib=rb-4.0.3&q=85)`
         document.getElementById('author').textContent = `By: JOHN TOWNER`;
     })
+;
 
-    //Fetch cyrpto currency data for bitcoin to access the image, name andd price
-    //Also have an error catching to provide an error
-    fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
-        .then(res => {
-            if(!res.ok) {
-                throw Error('Something went wrong');
-            }
-            return res.json()
-        })
-        .then(data => {
-            console.log(data)
-            document.getElementById('crypto-top').innerHTML = `
-                <img src=${data.image.small} />
-                <span>${data.name}</span>
-            `
-            document.getElementById("crypto").innerHTML += `
-                <p>🎯: R${data.market_data.current_price.zar}</p>
-                <p>👆: R${data.market_data.high_24h.zar}</p>
-                <p>👇: R${data.market_data.low_24h.zar}</p>
-            `
-        })
-        .catch(err => console.error(err))
+//Fetch cyrpto currency data for bitcoin to access the image, name andd price
+//Also have an error catching to provide an error
+fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+    .then(res => {
+        if(!res.ok) {
+            throw Error('Something went wrong');
+        }
+        return res.json();
+    })
+    .then(data => {
+        console.log(data)
+        document.getElementById('crypto-top').innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: R${data.market_data.current_price.zar}</p>
+            <p>👆: R${data.market_data.high_24h.zar}</p>
+            <p>👇: R${data.market_data.low_24h.zar}</p>
+        `;
+    })
+    .catch(err => console.error(err));
+;
+
+
+function getCurrentTime() {
+    const date = new Date();
+    document.getElementById('time').textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"});
+}
+
+setInterval(getCurrentTime, 1000);
